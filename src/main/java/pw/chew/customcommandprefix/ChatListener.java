b8327fd266e0de6197bec3ada0814ad4a6bd22d8
Brian <48810871+TBYT@users.noreply.github.com>
2023-06-19 21:03:41 -0700

    empty prefix, allow event instead of cancelling.

--- src/main/java/pw/chew/customcommandprefix/ChatListener.java
+++ src/main/java/pw/chew/customcommandprefix/ChatListener.java
@@ -6,18 +6,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
-    private final CustomCommandPrefix plugin;
+	private final CustomCommandPrefix plugin;
 
-    public ChatListener(CustomCommandPrefix plugin) {
-        this.plugin = plugin;
-    }
+	public ChatListener(CustomCommandPrefix plugin) {
+		this.plugin = plugin;
+	}
 
-    @EventHandler
-    public void onCommand(AsyncPlayerChatEvent event) {
-        String prefix = plugin.getConfig().getString("prefix");
-        if (event.getMessage().startsWith(prefix)) {
-            event.setCancelled(true);
-            Bukkit.getScheduler().runTask(plugin, () -> event.getPlayer().performCommand(event.getMessage().replace(prefix, "")));
-        }
-    }
+	@EventHandler
+	public void onCommand(AsyncPlayerChatEvent event) {
+		String prefix = plugin.getConfig().getString("prefix");
+		if (event.getMessage().startsWith(prefix)) {
+			event.setCancelled(true);
+			// logic for if prefix is set to be empty in the config, so we
+			// permit command debugging here.
+			if (prefix.isEmpty()) {
+				Bukkit.getScheduler().runTask(plugin, () -> event.getPlayer().performCommand(event.getMessage()));
+				event.setCancelled(false);
+				return;
+			}
+			Bukkit.getScheduler().runTask(plugin, () -> event.getPlayer().performCommand(event.getMessage().replace(prefix, "")));
+		}
+	}
 }
